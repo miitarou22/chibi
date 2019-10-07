53d43d9df9b812344bb9aff2bd9f6c7875d58b20
UEKI Miyu <21716017@jwu.ac.jp>
2019-10-07 16:51:44 +0900

    gcd

--- rational.py
+++ rational.py
@@ -1,7 +1,10 @@
+import math
+
 class Q(object):
     def __init__(self,a,b=1): 
-        self.a = a
-        self.b = b
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
 
     def __repr__(self):
         if self.b == 1:
