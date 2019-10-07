4e9b5ce752673852e233854d1eb9ff04d8bbd42a
UEKI Miyu <21716017@jwu.ac.jp>
2019-10-07 16:20:08 +0900

    rational3

--- rational.py
+++ rational.py
@@ -1,13 +1,21 @@
 class Q(object):
-    def __init__(self,a,b): 
-        self.a=a
-        self.b=b
+    def __init__(self,a,b=1): 
+        self.a = a
+        self.b = b
 
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
         return f'{self.a}/{self.b}'
 
-q = Q(3)
-print(q)  
+    def add(self,q):
+        a= self.a
+        b= self.b
+        c =q.a
+        d=q.b
+        return Q(a,b)
+
+q1 = Q(1,2)
+q2 = Q(1,3)
+print(q1.add(q2))  
     
