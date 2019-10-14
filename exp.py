044545cf28b2628a84c17840e42ebb2cb833605d
UEKI Miyu <21716017@jwu.ac.jp>
2019-10-14 16:21:08 +0900

    val2

--- exp.py
+++ exp.py
@@ -1,5 +1,9 @@
 
-class Val(object):
+class Expr(object):
+    pass
+
+
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self,value = 0):
         self.value = value
@@ -13,3 +17,28 @@ class Val(object):
 v =Val(1)
 print(v)
 assert v.eval() ==1
+
+assert isinstance(v, Expr) #==> True
+assert isinstance(v, Val) #==> True
+assert isinstance(v, int) #==> False
+
+class Add(Expr):
+    __slots__=['left','right']
+    def __init__(self,a,b):
+        self.left = a
+        self.right = b
+    
+    def eval(self):
+        return self.left.eval()+ self.right.eval()
+
+e = Add(Val(1),Val(2))
+assert e.eval() == 3
+
+e=Add(1,2)
+assert e.eval() ==3
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
+
+print()
+print()
