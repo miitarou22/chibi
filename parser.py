a0b338ac65bafde4c89321b0d5da50bf3ada64e7
UEKI Miyu <21716017@jwu.ac.jp>
2019-10-28 16:50:11 +0900

    tochuu

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val,Add
+from exp import Val,Add,Sub,Mul,Div
 
 def parse(s: str):
     num=int(s)
@@ -18,14 +18,28 @@ print(s,s1,s2) # +記号を探す
 '''
 
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        #num = int(s)
-        return Val(int(s))
-    else:
+    if s.find('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1),parse(s2))
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    if s.find('/') > 0:
+        pos = s.find('/')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1),parse(s2))
+    return Val(int(s))
 
-e = parse("1+2+3")
-print(e,e.eval())
+e = parse("1-2-3")
+print(e, e.eval()) # -4
+assert e.eval() == -4
