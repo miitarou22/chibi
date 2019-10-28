c2d539f20be7397759336fc76db840928e49d164
UEKI Miyu <21716017@jwu.ac.jp>
2019-10-28 15:50:35 +0900

    output  Val

---
+++ parser.py
@@ -0,0 +1,16 @@
+from exp import Val,Add
+
+def parse(s: str):
+    num=int(s)
+    return Val(num)
+
+e = parse("123")
+print(e)
+
+s = "123456"
+pos = s.find('+') # + 記号で探す
+print('pos', pos)
+
+s1 = s[0:pos]
+s2 = s[pos+1:]
+print(s,s1,s2) # +記号を探す
