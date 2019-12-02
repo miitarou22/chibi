e18078df2cb9511b653e95f0a0298682c88d8a9a
UEKI Miyu <21716017@jwu.ac.jp>
2019-12-02 16:46:03 +0900

    chibi

--- icalc2.py
+++ icalc2.py
@@ -19,14 +19,6 @@ def calc(t):
     print(f'TODO{t.tag}')
     return 0
 
-#t = parser('1+2*3+4*5')
-#print(repr(t))
-#print(calc(t))
-
-def main():
-    s = input('$ ')
-    t = parser(s)
-    print(calc(t))
-
-if  __name__ == '__main__':
-    main()
+t = parser('1+2*3+4*5')
+print(repr(t))
+print(calc(t))
