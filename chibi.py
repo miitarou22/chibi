b8a17c5bc74da8eb4def9528cbcdc4fd3ed73e14
UEKI Miyu <21716017@jwu.ac.jp>
2019-12-16 16:41:11 +0900

    sum

--- chibi.py
+++ chibi.py
@@ -80,6 +80,8 @@ class Var(Expr):
     __slots__ = ['name']
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
     def eval(self, env: dict):
         if self.name in env:
             return env[self.name]
@@ -119,50 +121,41 @@ class If(Expr):
             return self.then.eval(env)
         else:
             return self.else_.eval(env)
-
 class Lambda(Expr):
-    __slots__ = ['name','body',]
-    def __init__(self,name,body):
+    __slots__ = ['name', 'body']
+    def __init__(self, name, body):
         self.name = name
         self.body = body
-        
-    def repr(self):
+    def __repr__(self):
         return f'λ{self.name} . {str(self.body)}'
-        
-    def eval(self,env):
-        pass
-        
-f = Lambda('x',Add(Var('x'),1))  #λx . x+1
-print(repr(f))
-
-def copy(env):
-    newenv ={}
+    def eval(self, env):
+        return self
+def copy(env): #環境をコピーすることでローカルスコープを作る
+    newenv = {}
     for x in env.keys():
         newenv[x] = env[x]
-    return env
-
+    return newenv
 class FuncApp(Expr):
-    __slots__ = ['func','param']
-    def __init__(self,func: Lambda,param):
+    __slots__ = ['func', 'param']
+    def __init__(self, func: Lambda, param):
         self.func = func
         self.param = Expr.new(param)
-
     def __repr__(self):
-        return f'({repr(self.func)})({repr(self.param)})'
-        
-    def eval(self,env):
-        v = self.param.eval(env)
-        name = self.func.name
-        env[name] = v
-        return self.func.body.eval(env)
-        
-e = FuncApp(f,Add(1,1))
-        
-print(e, '=>', e.eval({}))
-        
+        return f'({repr(self.func)}) ({repr(self.param)})'
+    def eval(self, env):
+        f = self.func.eval(env)
+        v = self.param.eval(env)  # パラメータを先に評価する
+        name = f.name # Lambda の変数名をとる
+        env = copy(env)  # 環境をコピーすることでローカルスコープを作る
+        env[name] = v   # 環境から引数を渡す
+        return f.body.eval(env)
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
+    if tree == 'FuncDecl':   # この２行を追加します
+        return Assign(str(tree[0]), Lambda(str(tree[1]), conv(tree[2])))
+    if tree == 'FuncApp':   # この２行を追加します
+        return FuncApp(conv(tree[0]), conv(tree[1]))
     if tree == 'If':
         return If(conv(tree[0]), conv(tree[1]), conv(tree[2]))
     if tree == 'While':
@@ -217,7 +210,5 @@ def main():
         return
 if __name__ == '__main__':
     main()
-
-
 
 
